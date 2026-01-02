785d297a420f6c408c6a3d2af659bdb7fcab7435
GODOFPPT <gordon_hu@qq.com>
2026-01-02 12:09:51 +0800

    Update API configuration

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const API_BASE_URL = 'https://api.nm-cv.com';
-export const API_BASE_URL = 'http://127.0.0.1:3000';
+export const API_BASE_URL = 'https://api.nm-cv.com';
+// export const API_BASE_URL = 'http://127.0.0.1:3000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
